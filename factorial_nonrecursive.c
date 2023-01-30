439a7a7a7a3dbcff66bfd07ea7d5359ddcce5639
MeenakshiReddy <120377728+MeenakshiReddy@users.noreply.github.com>
2023-01-30 14:32:54 +0530

    Add files via upload

---
+++ factorial_nonrecursive.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void fact(int);
+int main(){
+int n;
+printf("enter the number : ");
+scanf("%d",&n);
+fact(n);
+return 0;
+}
+void fact(int n){
+int res=1;
+for (int i=1;i<=n;i++){
+res=res*i;
+}
+printf("%d",res);
+return;
+}
+
+
+
+ 
